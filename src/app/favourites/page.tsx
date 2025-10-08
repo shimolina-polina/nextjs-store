8a7f4a4b26ba0d4c5270a40208552f4d22e19512
polina-shimolina <shimolinapk@gmail.com>
2025-10-09 00:28:13 +0300

    fix env var

--- src/app/favourites/page.tsx
+++ src/app/favourites/page.tsx
@@ -3,7 +3,7 @@ import { ProductsList } from "src/components/products-list/ProductsList";
 export const dynamic = 'force-dynamic';
 
 export default async function FavouritesPage() {
-    const favourites = (await (await fetch(`${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/api/products/favourites`)).json()).products;
+    const favourites = (await (await fetch(`${process.env.VERCEL_URL || 'http://localhost:3000'}/api/products/favourites`)).json()).products;
 
     return (
         <>
